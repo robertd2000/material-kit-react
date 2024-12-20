73a89e0a4ebaa06a49d74a4762b6d6a7a93303eb
robertd2000 <zdabaghov@mail.ru>
2024-12-20 16:07:51 +0300

    blog

--- src/routes/sections.tsx
+++ src/routes/sections.tsx
@@ -15,7 +15,6 @@ export const EmployeePage = lazy(() => import('src/pages/employees'));
 export const EmployeeCreatePage = lazy(() => import('src/pages/employees/create/employees-create'));
 export const EmployeeEditPage = lazy(() => import('src/pages/employees/edit/employees-edit'));
 export const SignInPage = lazy(() => import('src/pages/sign-in'));
-export const ProductsPage = lazy(() => import('src/pages/products'));
 export const Page404 = lazy(() => import('src/pages/page-not-found'));
 
 // ----------------------------------------------------------------------
@@ -48,7 +47,6 @@ export function Router() {
         { path: 'employees', element: <EmployeePage /> },
         { path: 'employees/create', element: <EmployeeCreatePage /> },
         { path: 'employees/:id', element: <EmployeeEditPage /> },
-        { path: 'products', element: <ProductsPage /> },
       ],
     },
     {
